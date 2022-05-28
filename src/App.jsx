1f3b32dd4abf5d576dd9e0a12eb23ec2e8bf9784
Brian Horowitz <Guitarrook94@gmail.com>
2022-05-28 19:00:43 -0400

    Fixed bugs related to components cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Helloworld from './components/Helloworld'
-import Counter from './components/Counter'
 import Numeralconverter from './components/Numeralconverter'
-import Input from './components/Input'
-import EffectTutorial from './components/EffectTutorial'
+
 
 function App() {
   return (
